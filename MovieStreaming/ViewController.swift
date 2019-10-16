be984cf47c0cc941c79126ce66f4cea8850f90b2
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2019-10-16 15:56:45 +0700

    Initial Commit

---
+++ MovieStreaming/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieStreaming
+//
+//  Created by Arifin Firdaus on 10/16/19.
+//  Copyright © 2019 arifinfrds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
