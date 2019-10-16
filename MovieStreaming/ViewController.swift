9e9ca0accd10e2d618dd332e6e28192beaa091b9
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2019-10-16 16:04:12 +0700

    Create Base Project and Display sample Streaming Video

--- MovieStreaming/ViewController.swift
+++ MovieStreaming/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
+    
+    private var player: AVPlayer?
+    private var playerViewController = AVPlayerViewController()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupPlayerViewController()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showPlayerViewController()
+    }
+    
+    private func setupPlayerViewController() {
+        let source = "http://techslides.com/demos/sample-videos/small.mp4"
+        guard let url = URL(string: source) else { return }
+        let player = AVPlayer(url: url)
+        playerViewController.player = player
+    }
+    
+    private func showPlayerViewController() {
+        present(playerViewController, animated: true) {
+            self.playerViewController.player?.play()
+        }
     }
 
 
